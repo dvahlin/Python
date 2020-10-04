043bdc19a35c7697d1daac094acddcbb21dffdad
dvahlin <39875016+dvahlin@users.noreply.github.com>
2020-10-04 14:08:54 +0000

    Update menuparse.py

--- menuparse.py
+++ menuparse.py
@@ -3,14 +3,15 @@
 #import pypdf
 import PyPDF2
 
-
 def extract_information(pdf_path):
     with open(pdf_path, 'rb') as f:
         read_pdf = PyPDF2.PdfFileReader(pdf_path)
         number_of_pages = read_pdf.getNumPages()
-        page = read_pdf.getPage(0)
-        page_content = page.extractText()
-    print(page_content)
+
+        for page_number in range(number_of_pages):
+            page = read_pdf.getPage(page_number)
+            page_content = page.extractText()
+        print(page_content)
 
 if __name__ == '__main__':
     path = '/root/menyparse/meny.pdf'
